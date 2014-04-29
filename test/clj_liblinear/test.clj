e5ac6ffbab5d42bb94c1c9baca45cc9d226cc87c
we <daniel.slutsky@gmail.com>
2014-04-29 13:59:15 +0300

    generalized and cleaned test code

--- test/clj_liblinear/test.clj
+++ test/clj_liblinear/test.clj
@@ -100,103 +100,98 @@ The intercept is specified in feature name :intercept."
   (is (almost-equal-maps {:intercept 0.7529187765874954, :y 0.8761760796248441, :x -1.9341912291944392}
                          {:intercept 0.7529187765874954, :y 0.8761760796248441, :x -1.9341912291944392})))
 
+(def train-data (generate-logistic-observations 400
+                                                {:x -2
+                                                 :y 1
+                                                 :intercept 1}
+                                                0))
+
+(defn regression-test-template [& regression-cases]
+  ;; Check the model coefficients for various training scenations:
+  (eval (concat `(clojure.test/are [training-parameters expected-coefficients]
+                   (almost-equal-maps (do
+                                        ;; Reset liblinear's PRNG
+                                        (clj-liblinear.core/reset-random)
+                                        ;; Train model and get coefficients
+                                        (clj-liblinear.core/get-coefficients (apply clj-liblinear.core/train
+                                                                                    (map :f train-data)
+                                                                                    (map :class train-data)
+                                                                                    training-parameters)))
+                                      expected-coefficients))
+                regression-cases)))
+
+
 (deftest regression-test-for-git-2266be6
-  (let [ ;; Prepare training data
-        train-data (generate-logistic-observations 400
-                                                   {:x -2
-                                                    :y 1
-                                                    :intercept 1}
-                                                   0)
-        ;; Reset liblinear's PRNG
-        _ (reset-random)
-        ;; Train model
-        model (train
-               (map :f train-data)
-               (map :class train-data)
-               :algorithm :l1lr
-               :c 10
-               :bias true)]
-    ;; Check the model coefficients for various training scenations:
-    (are [training-parameters expected-coefficients]
-      (almost-equal-maps (do
-                           ;; Reset liblinear's PRNG
-                           (reset-random)
-                           ;; Train model and get coefficients
-                           (get-coefficients (apply train
-                                                    (map :f train-data)
-                                                    (map :class train-data)
-                                                    training-parameters)))
-                         expected-coefficients)
-      ;; Test various combinations of algorithm (taken from the supported
-      ;; algotithms), c (taken from #{2, 1/2}) and bias (taken from
-      ;; #{1, true, -1}, which are supposed to mean the same.
-      [:algorithm :l2lr_primal
-       :c 2
-       :bias true]
-      {:intercept 0.8451298717761591,
-       :y 0.9234249966851159,
-       :x -2.0443777978405175}
-      [:algorithm :l2l2
-       :c 1/2
-       :bias 1]
-      {:intercept 0.30896403558307006,
-       :y 0.32897934116468414,
-       :x -0.7493852105195001}
-      [:algorithm :l2l2
-       :c 1/2
-       :bias -1]
-      {:y 0.3071169929022199,
-       :x -0.6748243079870543}
-      [:algorithm :l2l2_primal
-       :c 2
-       :bias 1]
-      {:intercept 0.29480508710912584,
-       :y 0.3137835397140601,
-       :x -0.7111387065073224}
-      [:algorithm :l2l2_primal
-       :c 2
-       :bias -1]
-      {:y 0.30033425626017296,
-       :x -0.662693956280086}
-      [:algorithm :l2l1
-       :c 1/2
-       :bias true]
-      {:intercept 0.620462367740796,
-       :y 0.734783428258156,
-       :x -1.6304922916122815}
-      [:algorithm :multi
-       :c 2
-       :bias true]
-      {:intercept -0.34464667291199613,
-       :y 0.3979628683341206,
-       :x -0.3979628683341171}
-      [:algorithm :l1l2_primal
-       :c 1/2
-       :bias 1]
-      {:intercept 0.2886149834553072,
-       :y 0.308149642263233,
-       :x -0.7236401689670875}
-      [:algorithm :l1l2_primal
-       :c 1/2
-       :bias -1]
-      {:y 0.300478785493661,
-       :x -0.6696216144029736}
-      [:algorithm :l1lr
-       :c 2
-       :bias 1]
-      {:intercept 0.7529187765874954,
-       :y 0.8761760796248441,
-       :x -1.9341912291944392}
-      [:algorithm :l1lr
-       :c 2
-       :bias -1]
-      {:y 0.8430193551171671,
-       :x -1.8029098028343824}
-      [:algorithm :l2lr
-       :c 1/2
-       :bias true]
-      {:intercept 0.7905463338806276,
-       :y 0.8582570400126215,
-       :x -1.917358197930745})))
+  ;; Test various combinations of algorithm (taken from the supported
+  ;; algotithms), c (taken from #{2, 1/2}) and bias (taken from
+  ;; #{1, true, -1}, which are supposed to mean the same.
+  (regression-test-template [:algorithm :l2lr_primal
+                             :c 2
+                             :bias true]
+                            {:intercept 0.8451298717761591,
+                             :y 0.9234249966851159,
+                             :x -2.0443777978405175}
+                            [:algorithm :l2l2
+                             :c 1/2
+                             :bias 1]
+                            {:intercept 0.30896403558307006,
+                             :y 0.32897934116468414,
+                             :x -0.7493852105195001}
+                            [:algorithm :l2l2
+                             :c 1/2
+                             :bias -1]
+                            {:y 0.3071169929022199,
+                             :x -0.6748243079870543}
+                            [:algorithm :l2l2_primal
+                             :c 2
+                             :bias 1]
+                            {:intercept 0.29480508710912584,
+                             :y 0.3137835397140601,
+                             :x -0.7111387065073224}
+                            [:algorithm :l2l2_primal
+                             :c 2
+                             :bias -1]
+                            {:y 0.30033425626017296,
+                             :x -0.662693956280086}
+                            [:algorithm :l2l1
+                             :c 1/2
+                             :bias true]
+                            {:intercept 0.620462367740796,
+                             :y 0.734783428258156,
+                             :x -1.6304922916122815}
+                            [:algorithm :multi
+                             :c 2
+                             :bias true]
+                            {:intercept -0.34464667291199613,
+                             :y 0.3979628683341206,
+                             :x -0.3979628683341171}
+                            [:algorithm :l1l2_primal
+                             :c 1/2
+                             :bias 1]
+                            {:intercept 0.2886149834553072,
+                             :y 0.308149642263233,
+                             :x -0.7236401689670875}
+                            [:algorithm :l1l2_primal
+                             :c 1/2
+                             :bias -1]
+                            {:y 0.300478785493661,
+                             :x -0.6696216144029736}
+                            [:algorithm :l1lr
+                             :c 2
+                             :bias 1]
+                            {:intercept 0.7529187765874954,
+                             :y 0.8761760796248441,
+                             :x -1.9341912291944392}
+                            [:algorithm :l1lr
+                             :c 2
+                             :bias -1]
+                            {:y 0.8430193551171671,
+                             :x -1.8029098028343824}
+                            [:algorithm :l2lr
+                             :c 1/2
+                             :bias true]
+                            {:intercept 0.7905463338806276,
+                             :y 0.8582570400126215,
+                             :x -1.917358197930745}))
 
 
