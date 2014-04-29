152026e0c62b15b984dcc7682bb4bafbbd461f5a
we <daniel.slutsky@gmail.com>
2014-04-29 18:27:30 +0300

    refactoring into protocols; compiles, but not complete yet

--- src/clj_liblinear/core.clj
+++ src/clj_liblinear/core.clj
@@ -13,51 +13,69 @@
 
 (set! *warn-on-reflection* true)
 
+(defprotocol FeatureRows
+  (construct-feature-nodes-arrays [this] "Construct an array of arrays of FeatureNode objects (representing rows).")
+  (get-dimensions [this] "Get all of the dimensions as a map of dimension -> index."))
 
-(defprotocol FeaturesDataset
-  (construct-features-rows [this] "Construct an array of arrays of Features (representing rows)."))
-
-(extend-protocol FeaturesDataset
+(extend-protocol FeatureRows
+  ;; a sequence of map/sets instances
   clojure.lang.ISeq
-  (construct-features-rows [this] nil)
+  (construct-feature-nodes-arrays [iseq] nil)
+  (get-dimensions [iseq]
+    (let [dimnames (cond (every? map? iseq) (into #{} (flatten (map keys iseq)))
+                         (every? set? iseq) (apply union iseq))]
+      (into {} (map vector
+                    dimnames
+                    (range 1
+                           (inc (count dimnames)))))))
+  ;; a core.matrix Dataset
   clojure.core.matrix.impl.dataset.DataSet
-  (construct-features-rows [this] nil))
+  (construct-feature-nodes-arrays [this] nil)
+  (get-dimensions [this] nil))
 
+(defprotocol FeatureRow
+  (construct-feature-nodes [this dimensions]
+    "Represent this as a sequence of FeatureNode objects."))
 
-(defn feature-nodes [x dimensions]
-  (cond
-   (map? x) (for [[k v] x :when (contains? dimensions k)] (FeatureNode. (get dimensions k) v))
-   (set? x) (for [v x :when (dimensions v)] (FeatureNode. (get dimensions v) 1))))
+(extend-protocol FeatureRow
+  ;; a map
+  clojure.lang.IPersistentMap
+  (construct-feature-nodes [this dimensions]
+    (for [[k v] this
+          :when (contains? dimensions k)]
+      (FeatureNode. (get dimensions k) v)))
+  ;; a set
+  clojure.lang.IPersistentSet
+  (construct-feature-nodes [this dimensions]
+    (for [v this
+          :when (dimensions v)]
+      (FeatureNode. (get dimensions v) 1))))
 
-(defn dimensions
-  "Get all of the dimensions in a collection of map/set instances, return a map of dimension -> index"
-  [xs]
-  (let [dimnames (cond (every? map? xs) (into #{} (flatten (map keys xs)))
-                       (every? set? xs) (apply union xs))]
-    (into {} (map vector dimnames (range 1 (inc (count dimnames)))))))
+(defn- construct-bias-feature [bias feature-index] (FeatureNode. feature-index bias))
 
-(defn- bias-feature [bias feature-index] (FeatureNode. feature-index bias))
-
-(defn- feature-array
-  "Features are sorted by index. If bias is active, an extra feature is added."
-  [bias dims instance]
-  (let [nodes (sort-by #(.index ^FeatureNode %)
-                       (feature-nodes instance
-                                      dims))]
+(defn construct-feature-nodes-array
+  "Given a FeatureRow, represent it as an array of FeatureNode objects,
+ordered by index (the order of dimensions) and including the bias if necessary.
+If bias is active, an extra feature is added."
+  [bias dimensions feature-row]
+  (let [ordered-nodes (sort-by #(.index ^FeatureNode %)
+                               (construct-feature-nodes feature-row
+                                                        dimensions))]
     (if (>= bias 0)
-      (into-array (concat nodes
-                          [(bias-feature bias
-                                         (inc (count dims)))]))
-      (into-array nodes))))
+      (into-array (concat ordered-nodes
+                          [(construct-bias-feature bias
+                                                   (inc (count dimensions)))]))
+      (into-array ordered-nodes))) )
 
-(defn- correct-predictions
+
+(defn- count-correct-predictions
   [target labels]
   (count (filter true? (map = target labels))))
 
 (defn train
   "Train a LIBLINEAR model on a collection of maps or sets, xs, and a collection of their integer classes, ys."
   [xs ys & {:keys [c eps algorithm bias cross-fold]
-                      :or {c 1, eps 0.1, algorithm :l2l2, bias -1, cross-fold nil}}]
+            :or {c 1, eps 0.1, algorithm :l2l2, bias -1, cross-fold nil}}]
   (let [params (new Parameter (condp = algorithm
                                 :l2lr_primal SolverType/L2R_LR
                                 :l2l2 SolverType/L2R_L2LOSS_SVC_DUAL
@@ -71,15 +89,16 @@
         bias 	   (if (= Boolean (type bias))
                      (if (true? bias) 1 -1)
                      (if (>= bias 0) bias -1))
-        dimensions (dimensions xs)
-        xs         (into-array (map #(feature-array bias dimensions %) xs))
+        dimensions (get-dimensions xs)
+        feature-nodes-arrays (construct-feature-nodes-arrays xs)  
+        ;;(into-array (map #(feature-array bias dimensions %) xs))
         ys         (into-array Double/TYPE ys)
         prob       (new Problem)]
 
-    (set! (.x prob) xs)
+    (set! (.x prob) feature-nodes-arrays)
     (set! (.y prob) ys)
     (set! (.bias prob) bias)
-    (set! (.l prob) (count xs))
+    (set! (.l prob) (count feature-nodes-arrays))
     (set! (.n prob) (+ (count dimensions) (if (>= bias 0) 1 0)))
     
     ;;Train and return the model
@@ -87,14 +106,16 @@
                         (let [target (make-array Double/TYPE (count ys))]
                           (Linear/crossValidation prob params cross-fold target)
                           (println (format "Cross Validation Accuracy = %g%%%n"
-                                     (* 100.0 (/ (correct-predictions target ys) (count target)))))
+                                           (* 100.0 (/ (count-correct-predictions target ys) (count target)))))
                           target))
      :liblinear-model (Linear/train prob params)
      :dimensions dimensions}))
 
-(defn predict [model x]
+(defn predict [model feature-row]
   (let [m ^Model (:liblinear-model model)]
-    (Linear/predict m (feature-array (.getBias m) (:dimensions model) x))))
+    (Linear/predict m (construct-feature-nodes-array (.getBias m)
+                                                     (:dimensions model)
+                                                     feature-row))))
 
 (defn save-model
   "Writes the model out to two files specified by the base-file-name which should be a path and base file name. The extention .bin is added to the serialized java model and .edn is added to the clojure dimensions data."
