a82721d4fdd7b416e1cf79d6491bb4e9f71ba350
we <daniel.slutsky@gmail.com>
2014-05-01 18:32:43 +0300

    refactored regression tests

--- test/clj_liblinear/test.clj
+++ test/clj_liblinear/test.clj
@@ -106,7 +106,7 @@ The intercept is specified in feature name :intercept."
                                                  :intercept 1}
                                                 0))
 
-(defn regression-test-template [& regression-cases]
+(defn regression-test-template [& test-cases]
   ;; Check the model coefficients for various training scenations:
   (eval (concat `(clojure.test/are [training-parameters expected-coefficients]
                    (almost-equal-maps (do
@@ -118,112 +118,119 @@ The intercept is specified in feature name :intercept."
                                                                                     (map :class train-data)
                                                                                     training-parameters)))
                                       expected-coefficients))
-                regression-cases)))
+                (apply concat test-cases))))
 
 
-(deftest regression-test-for-git-2266be6
+(def git-2266be6-test-cases
   ;; Test various combinations of algorithm (taken from the supported
   ;; algotithms), c (taken from #{2, 1/2}) and bias (taken from
   ;; #{1, true, -1}.
-  (regression-test-template [:algorithm :l2lr_primal
-                             :c 2
-                             :bias true]
-                            {:intercept 0.8451298717761591, :y 0.9234249966851159, :x -2.0443777978405175}
-                            [:algorithm :l2l2
-                             :c 1/2
-                             :bias 1]
-                            {:intercept 0.30896403558307006, :y 0.32897934116468414, :x -0.7493852105195001}
-                            [:algorithm :l2l2
-                             :c 1/2
-                             :bias -1]
-                            {:y 0.3071169929022199, :x -0.6748243079870543}
-                            [:algorithm :l2l2_primal
-                             :c 2
-                             :bias 1]
-                            {:intercept 0.29480508710912584, :y 0.3137835397140601, :x -0.7111387065073224}
-                            [:algorithm :l2l2_primal
-                             :c 2
-                             :bias -1]
-                            {:y 0.30033425626017296, :x -0.662693956280086}
-                            [:algorithm :l2l1
-                             :c 1/2
-                             :bias true]
-                            {:intercept 0.620462367740796, :y 0.734783428258156, :x -1.6304922916122815}
-                            [:algorithm :multi
-                             :c 2
-                             :bias true]
-                            {:intercept -0.34464667291199613, :y 0.3979628683341206, :x -0.3979628683341171}
-                            [:algorithm :l1l2_primal
-                             :c 1/2
-                             :bias 1]
-                            {:intercept 0.2886149834553072, :y 0.308149642263233, :x -0.7236401689670875}
-                            [:algorithm :l1l2_primal
-                             :c 1/2
-                             :bias -1]
-                            {:y 0.300478785493661, :x -0.6696216144029736}
-                            [:algorithm :l1lr
-                             :c 2
-                             :bias 1]
-                            {:intercept 0.7529187765874954, :y 0.8761760796248441, :x -1.9341912291944392}
-                            [:algorithm :l1lr
-                             :c 2
-                             :bias -1]
-                            {:y 0.8430193551171671, :x -1.8029098028343824}
-                            [:algorithm :l2lr
-                             :c 1/2
-                             :bias true]
-                            {:intercept 0.7905463338806276, :y 0.8582570400126215, :x -1.917358197930745}))
+  [[[:algorithm :l2lr_primal
+     :c 2
+     :bias true]
+    {:intercept 0.8451298717761591, :y 0.9234249966851159, :x -2.0443777978405175}]
+   [[:algorithm :l2l2
+     :c 1/2
+     :bias 1]
+    {:intercept 0.30896403558307006, :y 0.32897934116468414, :x -0.7493852105195001}]
+   [[:algorithm :l2l2
+     :c 1/2
+     :bias -1]
+    {:y 0.3071169929022199, :x -0.6748243079870543}]
+   [[:algorithm :l2l2_primal
+     :c 2
+     :bias 1]
+    {:intercept 0.29480508710912584, :y 0.3137835397140601, :x -0.7111387065073224}]
+   [[:algorithm :l2l2_primal
+     :c 2
+     :bias -1]
+    {:y 0.30033425626017296, :x -0.662693956280086}]
+   [[:algorithm :l2l1
+     :c 1/2
+     :bias true]
+    {:intercept 0.620462367740796, :y 0.734783428258156, :x -1.6304922916122815}]
+   [[:algorithm :multi
+     :c 2
+     :bias true]
+    {:intercept -0.34464667291199613, :y 0.3979628683341206, :x -0.3979628683341171}]
+   [[:algorithm :l1l2_primal
+     :c 1/2
+     :bias 1]
+    {:intercept 0.2886149834553072, :y 0.308149642263233, :x -0.7236401689670875}]
+   [[:algorithm :l1l2_primal
+     :c 1/2
+     :bias -1]
+    {:y 0.300478785493661, :x -0.6696216144029736}]
+   [[:algorithm :l1lr
+     :c 2
+     :bias 1]
+    {:intercept 0.7529187765874954, :y 0.8761760796248441, :x -1.9341912291944392}]
+   [[:algorithm :l1lr
+     :c 2
+     :bias -1]
+    {:y 0.8430193551171671, :x -1.8029098028343824}]
+   [[:algorithm :l2lr
+     :c 1/2
+     :bias true]
+    {:intercept 0.7905463338806276, :y 0.8582570400126215, :x -1.917358197930745}]])
 
 
-(deftest regression-test-for-git-e5ac6ff
+(def git-e5ac6ff-test-cases
   ;; Test various combinations of algorithm (taken from the supported
   ;; algotithms), c (taken from #{2, 1/2}) and bias (taken from
   ;; #{false, 0, 1/2, 2}.
-  (regression-test-template [:algorithm :l2lr_primal
-                             :c 2
-                             :bias false]
-                            {:y 0.8526765355183481, :x -1.8337994601561565}
-                            [:algorithm :l2l2
-                             :c 1/2
-                             :bias 0]
-                            {:y 0.3071169929022199, :x -0.6748243079870543}
-                            [:algorithm :l2l2
-                             :c 1/2
-                             :bias 1/2]
-                            {:intercept 0.3052391093981819, :y 0.3316509018715258, :x -0.7459067206733213}
-                            [:algorithm :l2l2_primal
-                             :c 2
-                             :bias 2]
-                            {:intercept 0.2982394215552014, :y 0.3212333172919317, :x -0.7092266053362317}
-                            [:algorithm :l2l2_primal
-                             :c 2
-                             :bias false]
-                            {:y 0.30033425626017296, :x -0.662693956280086}
-                            [:algorithm :l2l1
-                             :c 1/2
-                             :bias 0]
-                            {:y 0.6069312911692889, :x -1.3852881423300722}
-                            [:algorithm :multi
-                             :c 2
-                             :bias 1/2]
-                            {:intercept -0.32057433223338094, :y 0.4075424968639208, :x -0.40754249686392163}
-                            [:algorithm :l1l2_primal
-                             :c 1/2
-                             :bias 2]
-                            {:intercept 0.2902361764826205, :y 0.30826752895573417, :x -0.7241907729810667}
-                            [:algorithm :l1l2_primal
-                             :c 1/2
-                             :bias false]
-                            {:y 0.300478785493661, :x -0.6696216144029736}
-                            [:algorithm :l1lr
-                             :c 2
-                             :bias 0]
-                            {:y 0.8302090159480037, :x -1.7793320288789982}
-                            [:algorithm :l1lr
-                             :c 2
-                             :bias 1/2]
-                            {:intercept 0.7441257065436431, :y 0.8748933116220571, :x -1.9307406585250886}
-                            [:algorithm :l2lr
-                             :c 1/2
-                             :bias 2]
-                            {:intercept 0.8126707014260155, :y 0.8625038218399448, :x -1.9281362351975666}))
+  [[[:algorithm :l2lr_primal
+      :c 2
+     :bias false]
+    {:y 0.8526765355183481, :x -1.8337994601561565}]
+   [[:algorithm :l2l2
+      :c 1/2
+     :bias 0]
+    {:y 0.3071169929022199, :x -0.6748243079870543}]
+   [[:algorithm :l2l2
+      :c 1/2
+     :bias 1/2]
+    {:intercept 0.3052391093981819, :y 0.3316509018715258, :x -0.7459067206733213}]
+   [[:algorithm :l2l2_primal
+      :c 2
+     :bias 2]
+    {:intercept 0.2982394215552014, :y 0.3212333172919317, :x -0.7092266053362317}]
+   [[:algorithm :l2l2_primal
+      :c 2
+     :bias false]
+    {:y 0.30033425626017296, :x -0.662693956280086}]
+   [[:algorithm :l2l1
+      :c 1/2
+     :bias 0]
+    {:y 0.6069312911692889, :x -1.3852881423300722}]
+   [[:algorithm :multi
+      :c 2
+     :bias 1/2]
+    {:intercept -0.32057433223338094, :y 0.4075424968639208, :x -0.40754249686392163}]
+   [[:algorithm :l1l2_primal
+      :c 1/2
+     :bias 2]
+    {:intercept 0.2902361764826205, :y 0.30826752895573417, :x -0.7241907729810667}]
+   [[:algorithm :l1l2_primal
+      :c 1/2
+     :bias false]
+    {:y 0.300478785493661, :x -0.6696216144029736}]
+   [[:algorithm :l1lr
+      :c 2
+     :bias 0]
+    {:y 0.8302090159480037, :x -1.7793320288789982}]
+   [[:algorithm :l1lr
+      :c 2
+     :bias 1/2]
+    {:intercept 0.7441257065436431, :y 0.8748933116220571, :x -1.9307406585250886}]
+   [[:algorithm :l2lr
+      :c 1/2
+     :bias 2]
+    {:intercept 0.8126707014260155, :y 0.8625038218399448, :x -1.9281362351975666}]])
+
+
+
+(deftest regression-test
+  (apply regression-test-template
+         (concat git-2266be6-test-cases
+                 git-e5ac6ff-test-cases)))
