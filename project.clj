3795d93206ba2a67ee9be4242256c54a0ddbf544
we <daniel.slutsky@gmail.com>
2014-05-05 11:43:54 +0300

    changed project's coordinates

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-liblinear "0.1.1-SNAPSHOT"
+(defproject daslu/clj-liblinear "0.1.2"
   :description "A Clojure wrapper for LIBLINEAR, a linear support vector machine library."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [de.bwaldvogel/liblinear "1.94"]
