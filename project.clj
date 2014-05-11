6d34346ec0e4519d1284acc17cb1f41ecedc0e74
we <daniel.slutsky@gmail.com>
2014-05-11 16:05:47 +0300

    updated dependencies

--- project.clj
+++ project.clj
@@ -2,4 +2,4 @@
   :description "A Clojure wrapper for LIBLINEAR, a linear support vector machine library."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [de.bwaldvogel/liblinear "1.94"]
-                 [net.mikera/core.matrix "0.22.0"]])
+                 [net.mikera/core.matrix "0.23.0"]])
