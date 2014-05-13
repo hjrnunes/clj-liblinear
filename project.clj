d00cc35316516f1e85198947477854b262dd413a
we <daniel.slutsky@gmail.com>
2014-05-13 11:40:06 +0300

    added clatrix as dependency

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "A Clojure wrapper for LIBLINEAR, a linear support vector machine library."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [de.bwaldvogel/liblinear "1.94"]
-                 [net.mikera/core.matrix "0.23.0"]])
+                 [net.mikera/core.matrix "0.23.0"]
+                 [clatrix "0.3.0"]])
