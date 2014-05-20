85a80d275ef56ddd9bb70cef434b2502bc92eea6
daslu <daniel.slutsky@gmail.com>
2014-05-20 16:29:38 +0300

    incremented artifact coordinates

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject daslu/clj-liblinear "0.1.3"
+(defproject daslu/clj-liblinear "0.1.4"
   :description "A Clojure wrapper for LIBLINEAR, a linear support vector machine library."
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [de.bwaldvogel/liblinear "1.94"]
