b0a3bcf2724ee9e166c13393ec9efa418160b21a
Henrique Nunes <henrique@chatterbox.co>
2015-04-08 02:19:13 +0100

    Fix predict. Update release.

--- project.clj
+++ project.clj
@@ -1,14 +1,14 @@
-(defproject hjrnunes/clj-liblinear "0.1.4"
+(defproject hjrnunes/clj-liblinear "0.1.5"
             :description "A Clojure wrapper for LIBLINEAR, a linear support vector machine library."
-            :url "https://github.com/daslu/clj-liblinear"
+            :url "https://github.com/hjrnunes/clj-liblinear"
             :license {:name "Eclipse Public License - v 1.0"
                       :url  "https://www.eclipse.org/legal/epl-v10.html"}
             :scm {:name "git"
-                  :url  "https://github.com/daslu/clj-liblinear"}
+                  :url  "https://github.com/hjrnunes/clj-liblinear"}
             :signing {:gpg-key "8896C73B"}
             :deploy-repositories [["clojars" {:creds :gpg}]]
 
-            :dependencies [[org.clojure/clojure "1.5.1"]
+            :dependencies [[org.clojure/clojure "1.6.0"]
                            [de.bwaldvogel/liblinear "1.94"]
                            [net.mikera/core.matrix "0.23.0"]
                            [clatrix "0.3.0"]])
--- src/clj_liblinear/core.clj
+++ src/clj_liblinear/core.clj
@@ -18,20 +18,20 @@
 
 
 (defrecord IndexedValues
-    ;; "a sequence of values, together with an index -- a map from each value to its ordinal number"
-    [^IPersistentVector values
-     ^IPersistentMap index])
+  ;; "a sequence of values, together with an index -- a map from each value to its ordinal number"
+  [^IPersistentVector values
+   ^IPersistentMap index])
 
 (defn indexed-values
   ([values index]
-     (IndexedValues. values
-                     index))
+   (IndexedValues. values
+                   index))
   ([values]
-     (indexed-values values
-                     (into {} (map vector
-                                   values
-                                   (range 1
-                                          (inc (count values))))))))
+   (indexed-values values
+                   (into {} (map vector
+                                 values
+                                 (range 1
+                                        (inc (count values))))))))
 
 (defn add [^IndexedValues ivs
            new-val]
@@ -47,7 +47,7 @@
 
 (defprotocol FeatureRow
   (construct-feature-nodes [this dimensions]
-    "Represent this as a sequence of FeatureNode objects."))
+                           "Represent this as a sequence of FeatureNode objects."))
 
 (defn- construct-bias-feature [bias feature-index] (FeatureNode. feature-index bias))
 
@@ -123,7 +123,7 @@ If bias is active, an extra feature is added."
 (defn train
   "Train a LIBLINEAR model on a collection of maps or sets, xs, and a collection of their integer classes, ys."
   [xs ys & {:keys [c eps algorithm bias cross-fold keep-nan-ys]
-            :or {c 1, eps 0.1, algorithm :l2l2, bias -1, cross-fold nil, keep-nan-ys false}}]
+            :or   {c 1, eps 0.1, algorithm :l2l2, bias -1, cross-fold nil, keep-nan-ys false}}]
   (let [params (new Parameter (condp = algorithm
                                 :l2lr_primal SolverType/L2R_LR
                                 :l2l2 SolverType/L2R_L2LOSS_SVC_DUAL
@@ -134,47 +134,47 @@ If bias is active, an extra feature is added."
                                 :l1lr SolverType/L1R_LR
                                 :l2lr SolverType/L2R_LR)
                     c eps)
-        bias 	   (if (= Boolean (type bias))
-                     (if (true? bias) 1 -1)
-                     (if (>= bias 0) bias -1))
+        bias (if (= Boolean (type bias))
+               (if (true? bias) 1 -1)
+               (if (>= bias 0) bias -1))
         dimensions (get-dimensions xs)
-        xs         (construct-feature-nodes-arrays xs
-                                                   bias
-                                                   dimensions)  
-        ys         (double-array ys)
+        xs (construct-feature-nodes-arrays xs
+                                           bias
+                                           dimensions)
+        ys (double-array ys)
         ;; handling the case of nan ys (interpreted as multiple
         ;; classes if kept)
-        [xs ys]    (if keep-nan-ys
-                     [xs ys]
-                     ;; else -- remove cases where y is nan
-                     (->> (map vector xs ys)
-                          (filter #(not (Double/isNaN (second %))))
-                          ((fn [xys]
-                              [(into-array (map first xys))
-                               (double-array (map second xys))]))))
-        prob       (new Problem)]
+        [xs ys] (if keep-nan-ys
+                  [xs ys]
+                  ;; else -- remove cases where y is nan
+                  (->> (map vector xs ys)
+                       (filter #(not (Double/isNaN (second %))))
+                       ((fn [xys]
+                          [(into-array (map first xys))
+                           (double-array (map second xys))]))))
+        prob (new Problem)]
 
     (set! (.x prob) xs)
     (set! (.y prob) ys)
     (set! (.bias prob) bias)
     (set! (.l prob) (count xs))
     (set! (.n prob) (+ (count (:index dimensions)) (if (>= bias 0) 1 0)))
-    
+
     ;;Train and return the model
-    {:target          (when cross-fold 
+    {:target          (when cross-fold
                         (let [target (make-array Double/TYPE (count ys))]
                           (Linear/crossValidation prob params cross-fold target)
                           (println (format "Cross Validation Accuracy = %g%%%n"
                                            (* 100.0 (/ (count-correct-predictions target ys) (count target)))))
                           target))
      :liblinear-model (Linear/train prob params)
-     :dimensions dimensions}))
+     :dimensions      dimensions}))
 
 (defn predict [model feature-row]
   (let [m ^Model (:liblinear-model model)]
-    (Linear/predict m (construct-feature-nodes-array (.getBias m)
-                                                     (:dimensions model)
-                                                     feature-row))))
+    (Linear/predict m (construct-feature-nodes-array feature-row
+                                                     (.getBias m)
+                                                     (:dimensions model)))))
 
 (defn save-model
   "Writes the model out to two files specified by the base-file-name which should be a path and base file name. The extention .bin is added to the serialized java model and .edn is added to the clojure dimensions data."
@@ -184,7 +184,7 @@ If bias is active, an extra feature is added."
   (let [dimensions (:dimensions model)]
     (spit (str base-file-name ".edn")
           {:values (:values dimensions)
-           :index (:index dimensions)})))
+           :index  (:index dimensions)})))
 
 (defn load-model
   "Reads a useable model from a pair of files specified by base-file-name. A file with the .bin extension should contain the serialized java model and the .edn file should contain the serialized (edn) clojure dimensions data."
@@ -212,16 +212,16 @@ The intercept corresponds to the key :intercept."
         ;; Get the indices (in the above ordering) corresponding to
         ;; the various feature names.
         feature-indices (:index
-                         (if include-bias
-                           (add (:dimensions model)
-                                :bias)
-                           (:dimensions model)))
+                          (if include-bias
+                            (add (:dimensions model)
+                                 :bias)
+                            (:dimensions model)))
         ;; Create a hashmap containing the coefficients by name.
         feature-coefficients (into {}
                                    (for [[feature-name feature-index] feature-indices
                                          :let [coefficient (coefficients-vector
-                                                            ;; dec, to start from 0, not 1
-                                                            (dec feature-index))]
+                                                             ;; dec, to start from 0, not 1
+                                                             (dec feature-index))]
                                          :when (not (zero? coefficient))]
                                      [feature-name coefficient]))]
     ;; Return the coefficients.
